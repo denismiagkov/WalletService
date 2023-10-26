bf64b4af75ded2a2d09460da5a45f57715c757f9
Denis Miagkov <tomich-84@mail.ru>
2023-10-27 00:52:26 +0300

    ServletContextListener implemented

--- src/main/java/com/denismiagkov/walletservice/infrastructure/in/servlets/AppServletContextListener.java
+++ src/main/java/com/denismiagkov/walletservice/infrastructure/in/servlets/AppServletContextListener.java
@@ -1,18 +1,20 @@
 package com.denismiagkov.walletservice.infrastructure.in.servlets;
 
+import com.denismiagkov.walletservice.init.WebInit;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebListener;
 
-@WebServlet
+
+@WebListener
 public class AppServletContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-
+        WebInit.start();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        ServletContextListener.super.contextDestroyed(sce);
     }
 }
