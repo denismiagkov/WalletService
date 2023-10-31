0b63ec7a69929a844efcef5b150062a04d9e8c92
Denis Miagkov <tomich-84@mail.ru>
2023-11-01 02:38:31 +0300

    PlayerServiceImpl: dependencies injected. javadoc corrected

--- src/main/java/com/denismiagkov/walletservice/application/service/serviceImpl/PlayerServiceImpl.java
+++ src/main/java/com/denismiagkov/walletservice/application/service/serviceImpl/PlayerServiceImpl.java
@@ -1,17 +1,12 @@
 package com.denismiagkov.walletservice.application.service.serviceImpl;
 
-import com.denismiagkov.walletservice.application.dto.EntryDto;
 import com.denismiagkov.walletservice.application.dto.PlayerDto;
-import com.denismiagkov.walletservice.application.service.Service;
-import com.denismiagkov.walletservice.application.service.serviceImpl.exceptions.IncorrectLoginException;
-import com.denismiagkov.walletservice.application.service.serviceImpl.exceptions.IncorrectPasswordException;
 import com.denismiagkov.walletservice.application.service.serviceImpl.exceptions.LoginIsNotUniqueException;
 import com.denismiagkov.walletservice.application.service.serviceImpl.exceptions.PlayerAlreadyExistsException;
 import com.denismiagkov.walletservice.domain.model.Player;
 import com.denismiagkov.walletservice.domain.service.PlayerService;
 import com.denismiagkov.walletservice.repository.PlayerDAOImpl;
-
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Низкоуровневый сервис, реализующий методы, связанные с <strong>обработкой и манипуляцией данных об игроке</strong>.
@@ -20,32 +15,30 @@ import java.util.Map;
  */
 @org.springframework.stereotype.Service
 public class PlayerServiceImpl implements PlayerService {
-
-    PlayerDAOImpl pdi;
-
+    /**
+     * ДАО игрока
+     */
+    PlayerDAOImpl playerDAO;
 
     /**
      * Конструктор класса
      */
+    @Autowired
     public PlayerServiceImpl() {
-        this.pdi = new PlayerDAOImpl();
+        this.playerDAO = new PlayerDAOImpl();
     }
 
-    //    /**
-//     * Метод создает игрока, уникальную комбинацию идентификатора (логина) и пароля, необходимую для проведения
-//     * аутентификации игрока при использовании приложения, "привязывает" данную комбинацию к игроку.
-//     *
-//     * @param firstName имя игрока
-//     * @param lastName  фамилия игрока
-//     * @param email     электронная почта игрока
-//     * @param login     уникальный идентификатор игрока (логин)
-//     * @param password  идентифицирующий признак игрока (пароль)
-//     * @return новый игрок
-//     * @throws PlayerAlreadyExistsException в случае, если у игрока уже имеется учетная запись в системе и он
-//     *                                      пытается зарегистрироваться повторно
-//     * @throws LoginIsNotUniqueException    в случае, если логин, предложенный пользователем в процессе регистрации,
-//     *                                      уже зарегистрирован в системе за другим игроком
-//     */
+    /**
+     * Метод создает игрока, уникальную комбинацию идентификатора (логина) и пароля, необходимую для проведения
+     * аутентификации игрока при использовании приложения, "привязывает" данную комбинацию к игроку.
+     *
+     * @param playerDto ДТО игрока (данные, полученные от пользователя при регистрации)
+     * @return новый игрок
+     * @throws PlayerAlreadyExistsException в случае, если у игрока уже имеется учетная запись в системе и он
+     *                                      пытается зарегистрироваться повторно
+     * @throws LoginIsNotUniqueException    в случае, если логин, предложенный пользователем в процессе регистрации,
+     *                                      уже зарегистрирован в системе за другим игроком
+     */
     @Override
     public Player registerPlayer(PlayerDto playerDto) throws RuntimeException {
         if (isPlayerExist(new Player(playerDto.getName(), playerDto.getSurname(), playerDto.getEmail()))) {
@@ -54,75 +47,68 @@ public class PlayerServiceImpl implements PlayerService {
             throw new LoginIsNotUniqueException(playerDto.getLogin());
         } else {
             Player player = new Player(playerDto.getName(), playerDto.getSurname(), playerDto.getEmail());
-            player = pdi.savePlayer(player);
+            player = playerDAO.savePlayer(player);
             Entry entry = new Entry(player.getId(), playerDto.getLogin(), playerDto.getPassword());
-            pdi.saveEntry(entry);
+            playerDAO.saveEntry(entry);
             return player;
         }
     }
 
     /**
-     * Метод выполняет аутентификацию игрока путем сопоставления идентификатора (логина)
-     * и идентифицирующего признака (пароля) для решения вопроса о доступе пользователя в систему
-     * {@link Service#authorizePlayer(String, String)}.
-     * *
+     * Метод возвращает булевое значение, зарегистрирован ли определенный игрок в приложении
      *
-     * @param login    идентификатор игрока (логин)
-     * @param password идентифицирующий признак игрока (пароль)
-     * @return игрок, пытающийся войти в систему или совершить в ней определенное действие
-     * @throws IncorrectLoginException    в случае, если пользователем введен логин, не зарегистрированный в системе
-     * @throws IncorrectPasswordException в случае, если пользователем введен неверный пароль
+     * @param player игрок
+     * @return булевое значение
      */
-    public int authorizePlayer(EntryDto entryDto) throws RuntimeException {
-        if (!isLoginExist(entryDto.getLogin())) {
-            throw new IncorrectLoginException(entryDto.getLogin());
-        } else if (isPasswordCorrect(entryDto.getLogin(), entryDto.getPassword())) {
-            int playerId = pdi.getPlayerId(entryDto.getLogin());
-            return playerId;
-        } else {
-            throw new IncorrectPasswordException();
-        }
-    }
-
-
-    public int getPlayerId(String login) {
-        return pdi.getPlayerId(login);
-    }
-
     private boolean isPlayerExist(Player player) {
-        if (pdi.getAllPlayers().contains(player)) {
+        if (playerDAO.getAllPlayers().contains(player)) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Метод возвращает булевое значение, занят ли определенный логин в приложении
+     *
+     * @param login логин игрока
+     * @return булевое значение
+     */
     private boolean isLoginExist(String login) {
-        if (pdi.getAllEntries().containsKey(login)) {
+        if (playerDAO.getAllEntries().containsKey(login)) {
             return true;
         } else {
             return false;
         }
     }
 
-    private boolean isPasswordCorrect(String login, String password) {
-        if (pdi.getAllEntries().get(login).equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
+    /**
+     * Метод возвращает id игрока по его логину
+     *
+     * @param login логин игрока
+     * @return int id игрока
+     */
+    public int getPlayerId(String login) {
+        return playerDAO.getPlayerId(login);
     }
 
+    /**
+     * Метод возвращает игрока по его логину
+     *
+     * @param login логин игрока
+     * @return игрок
+     */
     public Player getPlayerByLogin(String login) {
-        return pdi.getPlayerByLogin(login);
+        return playerDAO.getPlayerByLogin(login);
     }
 
-    public Map<String, String> getAllEntries() {
-        return pdi.getAllEntries();
-    }
-
+    /**
+     * Метод возвращает комбинацию логин-пароль по логину игрока
+     *
+     * @param login логин игрока
+     * @return комбинация логин-пароль
+     */
     public Entry getEntryByLogin(String login) {
-        return pdi.getEntryByLogin(login);
+        return playerDAO.getEntryByLogin(login);
     }
-
 }
